ba07160f4076bfe50479a2df9ac133366ea0c64b
biancaa-elenaa <bardanbianca04@gmail.com>
2025-05-03 21:57:42 +0300

    am facut o parte din cerintele de la faza a 2-a

---
+++ monitor.c
@@ -0,0 +1,147 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <sys/stat.h> 
+#include <dirent.h>
+#include "treasure.h"
+#include "treasure.c"
+
+#define CMD_FILE "command_file.txt"
+
+volatile sig_atomic_t got_command = 0;
+volatile sig_atomic_t stop_signal = 0;
+
+void handle_sigusr1(int sig)
+{
+    got_command = 1;
+}
+
+void list_hunts()
+{
+    DIR *directory = opendir(".");
+    if(directory  == NULL)
+    {
+        perror("Eroare la deschiderea directorului principal\n");
+        return;
+    }
+
+    struct dirent *files;
+    while((files = readdir(directory)) != NULL)
+    {
+        if(files->d_type == DT_DIR && strcmp(files->d_name, ".") != 0 && strcmp(files->d_name, "..") != 0)
+        {
+            char treasure_file_path[100];
+            snprintf(treasure_file_path,sizeof(treasure_file_path), "%s/treasures.bin", files->d_name);
+
+            if(access(treasure_file_path, F_OK) == 0)
+            {
+                //treasure.bin exista deci e un hunt valid
+                int f = open(treasure_file_path, O_RDONLY);
+                if(f == -1)
+                {
+                    perror("eroare la deschiderea fisierului treasures.bin");
+                    continue ;
+                }
+
+                int count =0 ;
+                Treasure_t treasure;
+                while(read(f,&treasure,sizeof(Treasure_t)) == sizeof(Treasure_t))
+                {
+                    count ++;
+                }
+
+                close(f);
+                printf("Hunt: %s | Comori: %d\n", files->d_name,count);
+            }
+        }
+    }
+    closedir(directory);
+}
+
+void process_comand()
+{
+    char filepath[100];
+    snprintf(filepath,sizeof(CMD_FILE),CMD_FILE);
+
+    int op;
+    if((op = open(filepath, O_RDONLY)) == -1)
+    {
+        perror("Eroare deschidere fisier\n");
+        exit(-1);
+    }
+
+    char command[100];
+    read(op,command,sizeof(command));
+    close(op);
+
+    command[strcspn(command, "\n")] = 0;
+
+    if(strcmp(command, "list_hunts") == 0)
+    {
+        list_hunts();
+    }
+    else if(strstr(command, "list_treasures") != 0)
+    {
+        char *dup = strdup(command);
+        char *token = strtok(dup, " "); //list_treasures
+        token = strtok(NULL, " ");
+        token[strcspn(token, "\r\n")] = '\0';
+        printf("hunt_id primit %s\n", token);
+        if(token)
+        {
+            list_treasures(token);
+        }
+        else
+        {
+            printf("[Monitor] eroare hunt_id!\n");
+
+        }
+        free(dup);
+    }
+    else if(strcmp(command, "view_treasures") == 0)
+    {
+        //de facut acelasi lucru oarecum ca la list_treasures
+    }
+    else if(strcmp(command, "stop_monitor") == 0)
+    {
+        usleep(3000000);
+        exit(0);
+    }
+    else
+    {
+        printf("[Monitor] Comanda necunoscuta!\n");
+        
+    }
+    truncate(CMD_FILE,0);
+}
+
+int main()
+{
+    struct sigaction sa;
+    sa.sa_handler = handle_sigusr1;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    
+    if(sigaction(SIGUSR1, &sa, NULL) == -1)
+    {
+        perror("Eroare sigaction\n");
+        exit(-1);
+    }
+
+    printf("Monitorul a pornit cu PID %d\n", getpid());
+
+    while(1)
+    {
+        pause(); //asteapta dupa semnal
+        if ( got_command )
+        {
+            process_comand();
+            got_command=0;
+        }
+        usleep(1000000);
+    }
+}
---
+++ treasure_hub.c
@@ -0,0 +1,179 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <sys/stat.h> 
+#include <dirent.h>
+#include "treasure.h"
+
+#define CMD_FILE "command_file.txt"
+
+pid_t monitor_pid = -1;
+int monitor_exiting = 0;
+
+void sigchld_handler(int sig)
+{
+    int status;
+    waitpid(monitor_pid, &status, 0);
+    monitor_pid = -1;
+    monitor_exiting = 0;
+
+    printf("Monitorul a terminat cu statusul : %d\n", WEXITSTATUS(status));
+
+}
+
+void send_command(const char *cmd)
+{
+    if(monitor_pid <= 0 || monitor_exiting)
+    {
+        printf("Monitorul nu ruleaza\n");
+        return;
+    }
+
+    int fd = open(CMD_FILE, O_WRONLY | O_CREAT | O_TRUNC, 0777);
+    if(fd == -1)
+    {
+        printf("Eroare la deschiderea fisierului\n");
+        exit(-1);
+    }
+    int written = write(fd,cmd,strlen(cmd));
+    if(written == -1)
+    {
+        printf("Eroare la scriere\n");
+        close(fd);
+        return;
+    }
+    close(fd);
+
+    if(kill(monitor_pid, SIGUSR1) == -1)
+    {
+        perror("KILL");
+    }
+}
+
+void start_monitor()
+{
+    if(monitor_pid > 0)
+    {
+        printf("Monitorul ruleaza deja.\n");
+        return;
+    }
+
+    monitor_pid = fork();
+    if(monitor_pid < 0)
+    {
+        perror("eroare pid");
+        exit(-1);
+    }
+    if(monitor_pid == 0)
+    {
+        execl("./monitor", "monitor", NULL);
+        perror("exec"); // daca execl a esuat
+        exit(1);
+    }
+
+    printf("Monitorul a inceput cu PID %d\n", monitor_pid);
+
+}
+
+void stop_monitor()
+{
+    if(monitor_pid <= 0)
+    {
+        printf("Monitorul nu ruleaza.\n");
+        return ;
+    }
+
+    send_command("stop_monitor");
+    monitor_exiting = 1;
+
+}
+
+int main()
+{
+    struct sigaction sa;
+    sa.sa_handler = sigchld_handler;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+
+    if(sigaction(SIGCHLD, &sa, NULL) == -1)
+    {
+        perror("Eroare sigaction\n");
+        exit(-1);
+    }
+
+    char command[100];
+
+    while(1)
+    {
+        printf("treasure_hub> ");
+        scanf("%s", command);
+        command[strcspn(command,"\n")] = 0;
+
+        if(strcmp(command, "start_monitor") == 0)
+        {
+            start_monitor();
+        }
+        else if(strcmp(command, "list_hunts") == 0)
+        {
+            send_command(command);    
+        }
+        else if(strcmp(command, "list_treasures") == 0)
+        {
+            char hunt_id[30];
+            printf("Introduceti hunt-ul din care doriti listarea comorilor: ");
+            scanf("%s", hunt_id);
+            hunt_id[strcspn(hunt_id, "\n")] =0;
+
+            char full_command[200];
+            sprintf(full_command, "%s %s","list_treasures ", hunt_id);
+            send_command(full_command);
+        }
+        else if(strcmp(command, "view_treasure") == 0)
+        {
+            char hunt_id[30];
+            int treasure_id;
+            printf("Introduceti hunt-ul din care doriti vizualizarea comorii: ");
+            scanf("%s",hunt_id);
+            hunt_id[strcspn(hunt_id, "\n")] =0;
+            printf("Introduceti ID-ul comorii: ");
+            scanf("%d", &treasure_id);
+
+            char full_command[200];
+            sprintf(full_command, "%s %s %s","view_treasures ",hunt_id,treasure_id);
+            send_command(command);
+        }
+        else if(strcmp(command, "stop_monitor") == 0)
+        {
+            stop_monitor();
+        }
+        else if(strcmp(command, "exit") == 0)
+        {
+            if(monitor_pid > 0 && !monitor_exiting)
+            {
+                printf("Eroare: Monitorul inca ruleaza.\n");
+                printf("Utilizati stop_monitor inainte de aceasta comanda.\n");
+            }else{
+                break;
+            }
+        }
+        else
+        {
+            printf("Comanda gresita!\n");
+            printf("Incercati: \n");
+            printf("start_monitor -> Porneste Monitorul\n");
+            printf("list_hunts   -> Afiseaza comorile dintr-un hunt si numarul total de comori in fiecare\n");
+            printf("list_treasures <hunt_id>   -> Afiseaza detaliile despre toate comorile dintr-un hunt\n");
+            printf("view <hunt_id> <id> -> Afiseaza detaliile unei comori\n");
+            printf("stop_monitor -> Opreste monitorul\n");
+            printf("exit  -> Daca monitorul inca e pornit, afisaza eroare, altfel termina programul\n")
+            
+            
+        }
+    }
+
+    return 0;
+}
